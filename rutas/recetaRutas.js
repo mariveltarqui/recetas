c4947b4eb1834d286c364162fd37ea53751aa262
maribeltarqui <147779358+mariveltarqui@users.noreply.github.com>
2024-05-11 14:35:20 -0400

    CRUD Recetas

--- rutas/recetaRutas.js
+++ rutas/recetaRutas.js
@@ -11,4 +11,50 @@ rutas.get('/traerRecetas', async (req, res) => {
         res.status(500).json({mensaje: error.message});
     }
 });
+
+//endpoint 2 crear
+rutas.post('/crear',async (req, res)=> {
+const receta =new RecetaModel({
+    nombre: req.body.nombre,
+    ingredientes: req.body.ingredientes,
+    porciones: req.body.porciones
+})
+
+try {
+    const nuevaRecetas = await receta.save();
+    res.status(201).json(nuevaRecetas);
+}catch (error) {
+    res.status(400).json({mensaje: error.message})
+}
+
+});
+
+//endpoint 3 editar
+rutas.put('/editar/:id', async (req, res) => {
+    try {
+        const recetaEditada = await RecetaModel.findByIdAndUpdate(req.params.id, req.body, {new : true});
+        if (!recetaEditada)
+            return res.status(404).json({ mesaje : 'Receta no encontrada!!!'});
+        else
+            return res.status(201).json(recetaEditada);
+    } catch (error){
+        res.status(400).json({ mensaje : error.mensaje})
+    }
+})
+
+//endpoint 4 eliminar
+rutas.delete('/eliminar/:id', async (req, res) =>{
+    try {
+        const recetaEliminada = await RecetaModel.findByIdAndDelete(req.params.id);
+        if (!recetaEliminada)
+            return res.status(404).json({ mesaje : 'Receta no encontrada!!!'});
+        else
+            return res.json({mesaje : 'Receta eliminada'});
+    } 
+    catch (error){
+        res.status(500).json({ mensaje : error.mensaje})
+    }
+
+})
+
 module.exports = rutas;
